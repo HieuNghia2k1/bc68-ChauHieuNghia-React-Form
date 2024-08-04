1d2c1a5d3b8f18aac24b22032d347995d717eb40
HieuNghia2k1 <boybin1707@gmail.com>
2024-08-04 15:46:53 +0700

    sunday_2

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import DemoFormReact from "./components/DemoFormReact/DemoFormReact";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-  return (
-   
-  )
+  return <DemoFormReact />;
 }
 
-export default App
+export default App;
